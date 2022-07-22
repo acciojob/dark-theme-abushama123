91453c9c7a8901fa67b22371bf5c25b352bcd47c
Abushama Siddique <abushamasiddique175@gmail.com>
2022-07-22 20:31:58 +0530

    Assignment Done

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
 
+const main = document.getElementById("app");
+const button = document.getElementById("swap");
+const swapTheme = () => {
+    if(main.classList.contains("day")){
+        main.classList.remove("day");
+        main.classList.add("night");
+        button.classList.remove("button_day");
+        button.classList.add("button_night");
+    }
+    else{
+        main.classList.add("day");
+        main.classList.remove("night");
+        button.classList.add("button_day");
+        button.classList.remove("button_night");
+    }
+}
+
+button.addEventListener("click", swapTheme);
+
